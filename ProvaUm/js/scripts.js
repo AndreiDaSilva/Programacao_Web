aad327d7fb905076e43b0802714a8a73847cf9cf
rbkretzer <rafael.bkretzer@gmail.com>
2022-04-28 12:22:18 -0300

    ajustes scripts e tela de login

---
+++ ProvaUm/js/scripts.js
@@ -0,0 +1,19 @@
+import { MobileNavbar } from './mobile-navbar.js';
+//inicia o nav bar
+const mobileNavbar = new MobileNavbar('.mobile-menu', '.nav-list', '.nav-list li');
+mobileNavbar.init();
+
+//autentica o usuário
+
+function autenticaUsuario() {
+    const userName = document.getElementById('user-name').value
+    const userPassword = document.getElementById('user-password').value;
+    if (!userPassword || !userPassword) {
+        window.alert('Preencha os campos obrigatórios');
+        return;
+    }
+    document.cookie = `username=${userName};`;
+    document.cookie = `password=${userPassword};`;
+    window.alert('Usuário autenticado');
+    location.replace('index.html')
+}
